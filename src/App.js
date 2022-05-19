b07a8a87748d1e105fdb109e6106a97de51b43a0
moaksan <fbi3287@naver.com>
2022-05-19 09:20:57 +0900

    y

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import request from './components/api.js'
 
-const url= 'https://stdict.korean.go.kr/api/search.do'
-const parameter={
-  key: '3D851C07D05A4E0EA6F848288DA18A2D',
-  q: 'water'
-}
-const json= request({url, parameter})
+// const url= 'https://stdict.korean.go.kr/api/search.do'
+// const parameter={
+//   key: '3D851C07D05A4E0EA6F848288DA18A2D',
+//   q: 'water'
+// }
+// const json= request({url, parameter})
 
 function App() {
   return (
     <div className="App">
-      <span>${json}</span>
+      <span>wowowowowo</span>
     </div>
   );
 }
